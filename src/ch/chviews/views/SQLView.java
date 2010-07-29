20c52aebd03eff89313182d35ea52baa15c31f28
Andrey Grebeniuk <andrew.grebeniuk@gmail.com>
2010-07-29 22:10:26 +0300

    Scrolling for Java2SQL view was added.

--- src/ch/chviews/views/SQLView.java
+++ src/ch/chviews/views/SQLView.java
@@ -2,15 +2,11 @@ package ch.chviews.views;
 
 import java.io.IOException;
 
-import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.part.*;
-import org.eclipse.ui.texteditor.ITextEditor;
 import org.eclipse.jface.action.*;
-import org.eclipse.jface.text.TextSelection;
 import org.eclipse.ui.*;
 import org.eclipse.swt.SWT;
 
@@ -20,21 +16,6 @@ import ch.utils.eclipse.WorkbenchHelper;
 import ch.utils.eclipse.log.ILog;
 import ch.utils.eclipse.log.Logger;
 import ch.utils.swt.chDlgs;
-
-/**
- * This sample class demonstrates how to plug-in a new workbench view. The view
- * shows data obtained from the model. The sample creates a dummy model on the
- * fly, but a real implementation would connect to the model available either in
- * this or another plug-in (e.g. the workspace). The view is connected to the
- * model using a content provider.
- * <p>
- * The view uses a label provider to define how model objects should be
- * presented in the view. Each view can present the same model objects using
- * different labels and icons, if needed. Alternatively, a single label provider
- * can be shared between views in order to ensure that objects of the same type
- * are presented in the same way everywhere.
- * <p>
- */
 
 public class SQLView extends ViewPart
 {
@@ -61,7 +42,7 @@ public class SQLView extends ViewPart
      */
     public void createPartControl(Composite parent)
     {
-        viewer = new Text(parent, SWT.MULTI);
+        viewer = new Text(parent, SWT.MULTI|SWT.V_SCROLL|SWT.H_SCROLL);
         viewer.setEditable(false);
         // Cornsilk 255-248-220
         viewer.setBackground(new Color(parent.getDisplay(), 255, 248, 220));
